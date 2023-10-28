a842a63ca2a8e0a6c18683c89d13e24696978733
gogi999 <g.petrovic9@gmail.com>
2023-10-28 12:03:20 +0200

    Working with state - useState hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,28 @@
 import './ExpenseItem.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem({ amount, date, title }) {
+    const [changedTitle, setChangedTitle] = useState(title);
+
+    const changeTitleHandler = () => {
+        setChangedTitle('Updated');
+        console.log(changedTitle);
+    }
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{changedTitle}</h2>
                 <div className="expense-item__price">${amount}</div>
             </div>
+            <button onClick={changeTitleHandler}>
+                Change Title
+            </button>
         </Card>
     );
 }
